23ec6cb1166524f6b18b007efa5a30cac55e1b14
BranTran <45236414+BranTran@users.noreply.github.com>
2019-03-12 21:43:12 -0500

    3/12/19 9:45 So much more to do

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-function Init(){
-  console.log("Hello")
+window.onload = function(){
+  var list = document.getElementById("newsitems")
+  var news = "JSON/recentNews.json";
+/*
+  getNews(news,function(data){
+    var descript = document.createElement("dt");
+    descript.innerHTML = "data.date1.date";
+    for(var i = 0; i < data.date1.class.length; i++)
+    {
+      var detail = document.createElement("dd");
+      detail.innerHTML = "data.date1.class[i]";
+      descript.appendChild(detail);
+    }//for
+    list.appendChild(descript);
+  });
+//*/
+}
+
+function getNews(news,callback){
+  var req = new XMLHttpRequest();
+	req.onreadystatechange = function(){
+		if(req.readyState == 4 && req.status == 200)
+		{
+			var data = JSON.parse(req.response);
+			callback(data);
+		}//if
+	}//function
+	req.open("GET",news,true);
+	req.send();
 }
