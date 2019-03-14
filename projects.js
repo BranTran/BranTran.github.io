f8cd74acfda018b3573e24e86f3ac122a9e63237
BranTran <45236414+BranTran@users.noreply.github.com>
2019-03-14 11:00:58 -0500

    Things are just not going well

--- projects.js
+++ projects.js
@@ -1,18 +1,20 @@
 window.onload = function(){
   var projects = "https://brantran.github.io/JSON/projects.json";
   var anchor = document.getElementById("resumeContent");
+  console.log("Hello");
   getProjects(projects,function(data){
-    showResume(data,anchor);
+    showProjects(data,anchor);
   });
 //*/
 }
 
 function getProjects(url,callback){
+  console.log("Into the function")
   var req = new XMLHttpRequest();
 	req.onreadystatechange = function(){
 		if(req.readyState == 4 && req.status == 200)
 		{
-      console.log("We dwnld the data")
+      console.log("We dwnld the data");
 			var data = JSON.parse(req.response);
       console.log(data);
 			callback(data);
@@ -21,8 +23,17 @@ function getProjects(url,callback){
 	req.open("GET",url,true);
 	req.send();
 }
-function showResume(data,anchor)
+function showProjects(data,anchor)
 {
+  console.log("Inside the show");
+  var projects = Object.keys(data);
+  for(var i=0; i < projects.length;i++)
+  {
+//    console.log("What do we have here"+i);
+//    var names = ;
+  console.log(projects[i]);
+//    console.log(data.(Object.keys(data)[i]).title);
+  }
 //  console.log(data);
     /*
 
