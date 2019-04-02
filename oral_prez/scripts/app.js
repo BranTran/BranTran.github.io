baadc6cf43027950349cc00c31ab48de49102a6a
BranTran <45236414+BranTran@users.noreply.github.com>
2019-04-02 13:31:51 -0500

    updating main.htm

--- oral_prez/scripts/app.js
+++ oral_prez/scripts/app.js
@@ -2,19 +2,19 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider){
 	$routeProvider
 	.when("/!", {
-		templateUrl: "main.htm",
+		templateUrl: "https://brantran.github.io/oral_prez/main.htm",
 //		template: "<h1>There is something here</h1><p><bo dy></p><p>{{msg}}</p><p>end body</p>",
 		controller: "empty"
 	})
 	.when("/angular", {
 		//templateUrl: "framwork.htm",
-		templateUrl: "main.htm",
+		templateUrl: "https://brantran.github.io/oral_prez/main.htm",
 		//template: "<h1>There is something here</h1><p><bo dy></p><p>{{msg}}</p><p>end body</p>",
 		controller: "angular"
 	})
 	.when("/vue", {
 		//templateUrl: "framework.htm"
-		templateUrl: "main.htm",
+		templateUrl: "https://brantran.github.io/oral_prez/main.htm",
 		//template:"<h1>There is something here</h1><p><bo dy></p><p>{{msg}}</p><p>end body</p>",
 		controller: "vue"
 	})
